6146d1f4a17a0d756dd82eff82e0f9c8ce78a278
RodrigoServieres <hiperodri974@gmail.com>
2023-10-17 19:02:01 -0300

    navbar

---
+++ asset/js/navbar.js
@@ -0,0 +1,9 @@
+let navbar = `
+    <header>
+        <ul>
+            <li><a href="./pages/productos.html">Productos</a></li>
+            <li><a href="./pages/contacto.html">Contacto</a></li>
+        </ul>
+    </header>    
+`
+document.write(navbar);
