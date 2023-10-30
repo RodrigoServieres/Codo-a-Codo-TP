d2e8e37b3a712a7676e159f398fed4fe95ecf6a9
San-Roj <santiagorojas@live.com.ar>
2023-10-29 22:16:54 -0300

    version final

--- asset/js/navbar2.js
+++ asset/js/navbar2.js
@@ -1,8 +1,10 @@
 let navbar = `
     <header>
         <ul>
+        <li><a href="../index.html">Inicio</a></li>
             <li><a href="./cards.html">Productos</a></li>
             <li><a href="./contacto.html">Contacto</a></li>
+            <li><a href="./sucursales.html">Sucursales</a></li>
         </ul>
     </header>    
 `
